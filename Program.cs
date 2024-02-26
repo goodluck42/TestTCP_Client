a9927486883f4d6aa444aeb19f08de7c07d044b4
Alex <xrlogic1@gmail.com>
2024-02-26 20:50:27 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var clientToServerSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+var serverEndpoint = IPEndPoint.Parse("10.0.0.101:13374");
+
+clientToServerSocket.Connect(serverEndpoint);
+
+
+try
+{
+    //var buffer = new byte[2048];
+    while (true)
+    {
+        string message = Console.ReadLine()!;
+        var bytes = Encoding.UTF8.GetBytes(message);
+
+        clientToServerSocket.Send(bytes);
+    }
+}
+finally
+{
+    clientToServerSocket.Dispose();
+}
